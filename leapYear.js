72899a07d449bf1ab334eeb56d25e3febdaf977f
lianabdiansyah <baimtjg21@gmail.com>
2021-09-23 16:04:44 +0700

    memperbaiki let input1

--- leapYear.js
+++ leapYear.js
@@ -6,7 +6,7 @@ function leap() {
     let input1 = parseInt(document.getElementById('angka1').value);
     let input2 = parseInt(document.getElementById("angka2").value);
     
-    for (let input1 = 1900 + 4; input1 < input2 + 4; input1 += 4){
+    for (input1 = input1 + 4; input1 < input2 + 4; input1 += 4){
         result += input1 + ",";
         document.getElementById('hasil').innerText = result;
         // console.log(input1);
